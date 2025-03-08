352730d7f48d936a3747c220a93939801fabdb80
Berke Karaali <bkaraali18@gmail.com>
2025-03-09 01:51:25 +0300

    Recruitment Container Design Done

--- Js/index.js
+++ Js/index.js
@@ -14,3 +14,18 @@ document
   .addEventListener("mouseout", function () {
     popoverTrigger.hide(); // Hover'dan çıktığında popover'ı kapatıyoruz
   });
+
+//! SCROLL BTN CODE
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 100) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
